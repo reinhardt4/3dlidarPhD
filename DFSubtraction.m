4500f42f0be435bee9efcb4adc27d78e481d3677
Andrew Reinhardt <13773907+reinhardt4@users.noreply.github.com>
2021-09-04 09:39:43 -0400

    Add files via upload
    DFSubtraction.m
    Author: Dr. Andrew Reinhardt
    Date: 8/9/2021
     
    Purpose: Functional capability to perform dark-frame subtraction.
     
    input: ND-array being DFNUC corrected; input arrays should be at least 2D
    DFNUC_Thermal: DFNUC array, if this array is 2D perform basic dark-frame subtraction, if this array is 3D, perform thermal compensationrefs. "Dark non-uniformity correction and characterization of a 3D flash lidar camera, AD Reinhardt, D Miller, A Lee, C Bradley, PF McManamon Laser Radar Technology and Applications XXIII 10636, 1063608" and 
    "Thermal drift compensation in dark-frame non-uniformtiy correction for InGaAs PIN 3D flash lidar camera, AE Hecht, AD Reinhardt, C Bradley, PF McManamon Laser Radar Technology and Applications XXVI 11744, 117440B"
    
    ouput: DFNUC corrected ND-array

---
+++ DFSubtraction.m
@@ -0,0 +1,20 @@
+% DFSubtraction.m
+% Author: Dr. Andrew Reinhardt
+% Date: 8/9/2021
+% 
+% Purpose: Functional capability to perform dark-frame subtraction.
+% 
+% input: ND-array being DFNUC corrected; input arrays should be at least 2D
+% DFNUC_Thermal: DFNUC array, if this array is 2D perform basic dark-frame subtraction, if this array is 3D, perform thermal compensationrefs. "Dark non-uniformity correction and characterization of a 3D flash lidar camera, AD Reinhardt, D Miller, A Lee, C Bradley, PF McManamon Laser Radar Technology and Applications XXIII 10636, 1063608" and 
+% "Thermal drift compensation in dark-frame non-uniformtiy correction for InGaAs PIN 3D flash lidar camera, AE Hecht, AD Reinhardt, C Bradley, PF McManamon Laser Radar Technology and Applications XXVI 11744, 117440B"
+
+function output=DFSubtraction(input,DFNUC_Thermal)
+
+if isempty(size(DFNUC_Thermal,3))
+	%if the DFNUC array has no 3rd dimension, correct as if without thermal compensation, i.e. simple dark-frame subtraction
+	output = input - DFNUC_Thermal + mean(mean(DFNUC_Thermal,2),1);
+else
+	%if the DFNUC array has a third dimension, pass to the thermal IDX, and thermally compensated DFNUC
+	idx=thermalIDX(input,DFNUC_Thermal);
+	output = input - DFNUC_Thermal(:,:,idx) + mean(mean(DFNUC_Thermal(:,:,idx),2),1);
+end
