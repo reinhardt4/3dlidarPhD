b532280c62394238e553318d23b9949bcc7c2da4
Andrew Reinhardt <13773907+reinhardt4@users.noreply.github.com>
2021-09-04 09:40:17 -0400

    Update DFSubtraction.m

--- DFSubtraction.m
+++ DFSubtraction.m
@@ -7,6 +7,8 @@
 % input: ND-array being DFNUC corrected; input arrays should be at least 2D
 % DFNUC_Thermal: DFNUC array, if this array is 2D perform basic dark-frame subtraction, if this array is 3D, perform thermal compensationrefs. "Dark non-uniformity correction and characterization of a 3D flash lidar camera, AD Reinhardt, D Miller, A Lee, C Bradley, PF McManamon Laser Radar Technology and Applications XXIII 10636, 1063608" and 
 % "Thermal drift compensation in dark-frame non-uniformtiy correction for InGaAs PIN 3D flash lidar camera, AE Hecht, AD Reinhardt, C Bradley, PF McManamon Laser Radar Technology and Applications XXVI 11744, 117440B"
+%
+% output: DFNUC corrected ND-array
 
 function output=DFSubtraction(input,DFNUC_Thermal)
 
